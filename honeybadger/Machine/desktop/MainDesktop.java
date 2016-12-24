654f5d3a109921b5983ee7d0ae2514ce1ea534ac
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2016-12-24 03:17:16 -0500

    Network loops and retries incorporated on desktop.
    Now need to do some state machine for the pi.

--- honeybadger/Machine/desktop/MainDesktop.java
+++ honeybadger/Machine/desktop/MainDesktop.java
@@ -12,31 +12,44 @@ public class MainDesktop {
     //Quick test for network
     public static void main(String[] args){
         Scanner Kb = new Scanner(System.in);
-
-        String IP = "192.168.0.1";
-        Log(String.format("Enter RPi IP (Default %s): ",IP));
-        String input = Kb.nextLine();
-        if(!input.isEmpty()){
-            IP=input;
-        }
-
-        Log(String.format("Connecting to %s",IP));
-        Machine.desktop.NetworkConnector nc = new Machine.desktop.NetworkConnector(IP,2017);
-        Controller Xbox = new Controller(nc);
-        input="";
-        Log("Connection established");
-        while(true){
-            input = Prompt('>',Kb);
-            Log(String.format("Sending \"%s\"",input));
-            nc.SendMessage(input);
-            if(input.contains("quit")){
-                nc.End();
-                nc = null;
-                break;
+        boolean isActive=true;
+        do {
+            String IP = "192.168.0.1";
+            Log(String.format("Enter RPi IP (Default %s): ", IP));
+            String input = Kb.nextLine();
+            if (!input.isEmpty()) {
+                IP = input;
             }
-        }
 
-        Log("Closing Connection");
+            Log(String.format("Connecting to %s", IP));
+            Machine.desktop.NetworkConnector nc = new Machine.desktop.NetworkConnector(IP, 2017);
+            Controller Xbox = new Controller(nc);
+            input = "";
+
+            while (true) {
+                input = Prompt('>', Kb);
+                Log(String.format("Sending \"%s\"", input));
+                nc.SendMessage(input);
+
+                //Exit if we said "quit"
+                if (input.contains("quit")) {
+                    nc.End();
+                    nc = null;
+                    Log("Closing Connection");
+                    isActive=false;
+                    break;
+                }
+
+                //Retry if the connection broke
+                if (nc.IsBroken()) {
+                    nc.End();
+                    nc = null;
+                    Log("Lost Connection... Reconnecting");
+                    break;
+                }
+            }
+        }while(isActive);
+
         System.exit(0);
     }
 }
--- honeybadger/Machine/desktop/NetworkConnector.java
+++ honeybadger/Machine/desktop/NetworkConnector.java
@@ -6,12 +6,16 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import static Machine.Common.Utils.Log;
+
 /**
  * Creates a connection and sends messages down the wire
  */
 public class NetworkConnector {
     String host;
     int port;
+
+    boolean exceptionOccurred;
 
     Socket connection;
     ObjectOutputStream outStream;
@@ -21,28 +25,46 @@ public class NetworkConnector {
     NetworkConnector(String Host, int connectPort){
         host = Host;
         port = connectPort;
-        try{
-            connection = new Socket(host,port);
-            outStream = new ObjectOutputStream(connection.getOutputStream());
-            inStream = new ObjectInputStream(connection.getInputStream());
+        exceptionOccurred = false;
+
+        int retries=3;
+        while(connection==null && retries>0) {
+            try {
+                connection = new Socket(host, port);
+                outStream = new ObjectOutputStream(connection.getOutputStream());
+                inStream = new ObjectInputStream(connection.getInputStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.flush();
+                System.err.flush();
+                Log("waiting 10 seconds to retry");
+            }
+
+            if(connection==null){
+                try {
+                    Thread.sleep(10000);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    Log("Interrupted during retry wait period. Exiting");
+                    System.exit(1);
+                }
+                retries-=1;
+            }
         }
-        catch(Exception e){
-            e.printStackTrace();
-            System.exit(-1);
+
+        if(connection==null){
+            System.exit(1);
         }
+
+        Log(String.format("Connection to %s:%s established successfully",Host,connectPort));
         LastMessage = null;
     }
 
     void SendMessage(String msg){
         if(connection!=null){
             LastMessage = new BaseMsg(msg);
-            try {
-                outStream.writeObject(LastMessage);
-            }
-            catch (Exception e){
-                e.printStackTrace();
-                System.out.println("Message Not Sent: "+LastMessage.getPayload());
-            }
+            SendMessage(LastMessage);
         }
     }
 
@@ -53,6 +75,7 @@ public class NetworkConnector {
                 outStream.writeObject(LastMessage);
             }
             catch (Exception e){
+                exceptionOccurred = true;
                 e.printStackTrace();
                 System.out.println("Message Not Sent: "+LastMessage.getPayload());
             }
@@ -60,6 +83,10 @@ public class NetworkConnector {
     }
 
     void End(){
+        if(!connection.isConnected()){
+            return;
+        }
+
         try {
             connection.close();
         }
@@ -68,4 +95,7 @@ public class NetworkConnector {
         }
     }
 
+    public boolean IsBroken() {
+        return exceptionOccurred;
+    }
 }
