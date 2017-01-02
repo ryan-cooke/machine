0b4197f5a041cb1a9934e95ee4595ad4d73d2914
alex <alex93dussault@gmail.com>
2017-01-02 18:54:59 -0500

    Helper methods to change the controller state.

--- honeybadger/Machine/desktop/BadgerAutonomousController.java
+++ honeybadger/Machine/desktop/BadgerAutonomousController.java
@@ -26,7 +26,48 @@ public class BadgerAutonomousController {
      * Runs the autonomous script that begins sending messages to the RPi
      */
     public void TakeOver(){
-        //TODO: Place State machine logic here
 
+    }
+
+    /**
+     * Helper methods to move the badger in given directions, and to activate the motors.
+     * @param throttle: between 0-1.0, throttle they will be given.
+     */
+    public void moveForward(double throttle){
+        controllerState.leftThumbstickDirection = 'N';
+        controllerState.leftThumbstickMagnitude = throttle;
+    }
+
+    public void strafeLeft(double throttle){
+        controllerState.leftThumbstickDirection = 'W';
+        controllerState.leftThumbstickMagnitude = throttle;
+    }
+
+    public void strafeRight(double throttle){
+        controllerState.leftThumbstickDirection = 'E';
+        controllerState.leftThumbstickMagnitude = throttle;
+    }
+
+    public void moveBackwards(double throttle){
+        controllerState.leftThumbstickDirection = 'S';
+        controllerState.leftThumbstickMagnitude = throttle;
+    }
+
+    public void rotateRight(double throttle){
+        controllerState.rightThumbstickDirection = 'E';
+        controllerState.rightThumbstickMagnitude = throttle;
+    }
+
+    public void rotateLeft(double throttle){
+        controllerState.rightThumbstickDirection = 'W';
+        controllerState.rightThumbstickMagnitude = throttle;
+    }
+
+    public void flywheel(double throttle){
+        controllerState.rightTriggerMagnitude = throttle;
+    }
+
+    public void conveyors(double throttle){
+        controllerState.leftTriggerMagnitude = throttle;
     }
 }
