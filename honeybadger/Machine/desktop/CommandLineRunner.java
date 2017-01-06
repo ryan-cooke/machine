fd5fc846d64c4e939c862ff44d45738efb963915
Alex Stojda <alexstojda@outlook.com>
2017-01-06 10:54:55 -0500

    Quick pre-release fixes for XML write in command runner

--- honeybadger/Machine/desktop/CommandLineRunner.java
+++ honeybadger/Machine/desktop/CommandLineRunner.java
@@ -1,14 +1,15 @@
 package Machine.desktop;
 
 import Machine.Common.Shell;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,9 +24,9 @@ public class CommandLineRunner {
         final String netGateway = "192.168.0.1";
         final String netSubmask = "255.255.255.0";
 
-        try {
-            networkAdapterName = getNetworkCardName();
-        } catch (Exception e) {
+        networkAdapterName = getNetworkCardName();
+
+        if (networkAdapterName == null) {
             networkAdapterName = "Wi-Fi";
         }
 
@@ -52,18 +53,39 @@ public class CommandLineRunner {
         windows.Run(command,60000);
     }
 
-    private static String getNetworkCardName() throws ParserConfigurationException, IOException, SAXException {
-        File fXmlFile = new File("config.xml");
-        fXmlFile.createNewFile();
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(fXmlFile);
+    private static String getNetworkCardName() {
+        try {
+            File fXmlFile = new File("config.xml");
 
-        //optional, but recommended
-        //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-        doc.getDocumentElement().normalize();
-        Element netCard = (Element) doc.getElementsByTagName("networkCard").item(0);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            if(!fXmlFile.exists()){
+                Document doc = dBuilder.newDocument();
+                Element networkCard = doc.createElement("networkCard");
+                networkCard.setNodeValue("Wi-Fi");
+                doc.appendChild(networkCard);
 
-        return netCard.getTextContent();
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File("config.xml"));
+                transformer.transform(source, result);
+                return "Wi-Fi";
+            } else {
+                Document doc = dBuilder.parse(fXmlFile);
+
+                doc.getDocumentElement().normalize();
+                Element netCard = (Element) doc.getElementsByTagName("networkCard").item(0);
+
+                return netCard.getTextContent();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println(getNetworkCardName());
     }
 }
